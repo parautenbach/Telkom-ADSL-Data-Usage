84634f9431cb2e8b2ff8115ca2193ff7eeb97a48
parautenbach <parautenbach@gmail.com>
2017-03-06 20:23:01 +0200

    Usage vs remainder
    Set in config whether you’d like to see the usage or remainder.

--- telkom.py
+++ telkom.py
@@ -20,12 +20,16 @@ REGEX = REGEX + '.*?' + REGEX
 REFRESH_MENU = 'Refresh'
 REFRESH_INTERVAL = 10*60
 REMAINDER_KEY = 'remainder'
+USAGE_KEY = 'usage'
 USAGE_SUBSTRING = 'usage'
+TOGGLE_MENU = 'Toggle'
+TOGGLE_TEMPLATE = 'Show {}'
 UPDATING_MESSAGE = 'Updating...'
 ERROR_MESSAGE = 'Error'
 CONF_DEFAULT_SECTION = 'default'
 CONF_USERNAME = 'username'
 CONF_PASSWORD = 'password'
+CONF_TOGGLE = 'toggle'
 
 # Path constants
 CONF_TEMPLATE_PATH = '{0}/conf/{1}'
@@ -66,43 +70,55 @@ def extract_data(html):
         scripts.append(script)
     return scripts[7].text
 
-def parse_remainder(script):
+def parse_info(script):
     """
     Parse the remaining data from the given script tag.
 
-    :return: Tuple of (remain_label, data_string, data_percentage_string)
+    :return: Dictionary with usage and remainder containing tuples of (label, data_string, data_percentage_string)
     """
     match = re.search(REGEX, script, flags=re.MULTILINE|re.DOTALL)
     # we don't know which group is the usage and which the remainder
     if USAGE_SUBSTRING in match.groups()[4]:
         usage = match.groups()[3:6]
         remainder = match.groups()[0:3]
+        usage = match.groups()[3:6]
     else:
         usage = match.groups()[0:3]
         remainder = match.groups()[3:6]
-    return (remainder[1], remainder[2], remainder[0])
+    info = {
+        REMAINDER_KEY: (remainder[1], remainder[2], remainder[0]),
+        USAGE_KEY: (usage[1], usage[2], usage[0])
+    }
+    return info
 
 def reload_info():
     """
     Reload usage info.
     """
-    global app, logger, username, password
+    global app, logger, username, password, toggle
     try:
         old_title = app.title
         app.title = UPDATING_MESSAGE
         html = get_page(username, password)
         data = extract_data(html)
-        remainder = parse_remainder(data)
-        info = {
-            REMAINDER_KEY: remainder
-        }
-        logger.info(remainder[0] + ': ' +  remainder[1] + ' (' + remainder[2] + '%)')
-        app.title = '{0} ({1}%)'.format(info[REMAINDER_KEY][1], info[REMAINDER_KEY][2])
+        info = parse_info(data)
+        logger.info(info[REMAINDER_KEY][0] + ': ' +  info[REMAINDER_KEY][1] + ' (' + info[REMAINDER_KEY][2] + '%)')
+        logger.info(info[USAGE_KEY][0] + ': ' +  info[USAGE_KEY][1] + ' (' + info[USAGE_KEY][2] + '%)')
+        app.title = '{0} ({1}%)'.format(info[toggle][1], info[toggle][2])
     except Exception, e:
         logger.exception(e)
         app.title = ERROR_MESSAGE
 
-@rumps.clicked(REFRESH_MENU)
+def toggle():
+    """
+    Toggle between usage and remainder.
+    """
+    global app, toggle, logger
+    logger.info(app.menu[TOGGLE_MENU])
+    logger.info(toggle)
+    app.menu[TOGGLE_MENU].title = TOGGLE_TEMPLATE.format('foo')
+
+#@rumps.clicked(REFRESH_MENU)
 def refresh_callback(_):
     """
     Refresh menu item's click action.
@@ -110,6 +126,15 @@ def refresh_callback(_):
     global logger
     logger.info(REFRESH_MENU)
     reload_info()
+
+#@rumps.clicked(TOGGLE_MENU)
+def toggle_callback(_):
+    """
+    Toggle menu item's click action.
+    """
+    global logger
+    logger.info(TOGGLE_MENU)
+    toggle()
 
 @rumps.timer(REFRESH_INTERVAL)
 def reload_info_callback(sender):
@@ -132,22 +157,31 @@ def main():
     """
     Main application.
     """
-    global app, app_path, username, password, info
+    global app, app_path, username, password, toggle, info
     logger.info('Reading config')
     config_parser = ConfigParser.SafeConfigParser()
     with open(CONF_TEMPLATE_PATH.format(app_path, APP_CONF)) as config_file:
         config_parser.readfp(config_file)
         username = config_parser.get(CONF_DEFAULT_SECTION, CONF_USERNAME)
         password = config_parser.get(CONF_DEFAULT_SECTION, CONF_PASSWORD)
+        toggle = config_parser.get(CONF_DEFAULT_SECTION, CONF_TOGGLE)
     logger.info('Username: ' + username)
-    refresh = rumps.MenuItem(REFRESH_MENU, 
-                             icon=ICONS_TEMPLATE_PATH.format(app_path, REFRESH_ICON), 
-                             dimensions=(16, 16))
+    logger.info('Toggle: ' + toggle)
+    refresh_menu = rumps.MenuItem(REFRESH_MENU, 
+                                  callback=refresh_callback,
+                                  icon=ICONS_TEMPLATE_PATH.format(app_path, REFRESH_ICON), 
+                                  dimensions=(16, 16))
+    toggle_menu = rumps.MenuItem(TOGGLE_MENU, 
+                                 callback=toggle_callback)
     app = rumps.App(APP_NAME,
                     icon=ICONS_TEMPLATE_PATH.format(app_path, APP_ICON),
-                    menu=(refresh, None))
+                    menu=(refresh_menu, toggle_menu, None))
     logger.info('Running')
     app.title = APP_NAME
+    # app.menu = [
+    #     rumps.MenuItem(REFRESH_MENU, callback=refresh_callback),
+    #     rumps.MenuItem(TOGGLE_MENU, callback=toggle_callback)
+    # ]
     app.run()
 
 if __name__ == "__main__":
